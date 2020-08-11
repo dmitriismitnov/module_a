68582495c62fec369581e9c4c2bf205d498535d0
es <es@MacBook-Pro-mac.local>
2020-08-12 02:09:12 +0300

    changes

--- lib/src/module_a_base.dart
+++ lib/src/module_a_base.dart
@@ -1,5 +1,7 @@
+import 'package:module_common/module_common.dart';
+
 class ModuleA {
   static void sayHello() {
-    print('I\'am module module_a');
+    print('I\'am module module_a. ${ModuleCommon.moduleName}::${ModuleCommon.moduleVersion}');
   }
 }
