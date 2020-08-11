c283beec8d9588fbe19712d78525ecf1b6e31151
es <es@MacBook-Pro-mac.local>
2020-08-12 01:43:18 +0300

    init

---
+++ example/module_a_example.dart
@@ -0,0 +1,6 @@
+import 'package:module_a/module_a.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/module_a.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library module_a;
+
+export 'src/module_a_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
